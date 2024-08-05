2217b5a73adc7467da57127ba3905d400eb936e4
wjq <1084948949@qq.com>
2024-08-05 09:58:42 +0800

    框架整理，redis依赖配置

--- src/main/java/com/cyanrocks/wms/controller/HelloController.java
+++ src/main/java/com/cyanrocks/wms/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.cyanrocks.wms.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = {"测试接口"})
 public class HelloController {
 
     @GetMapping("/")
+    @ApiOperation(value = "测试")
     public String index() {
         return "Greetings from Spring Boot!";
     }
--- src/main/java/com/cyanrocks/wms/mapper/DashboardMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cyanrocks.wms.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DashboardMapper {
-}
