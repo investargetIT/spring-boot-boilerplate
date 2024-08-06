a36530434a977ed1c716542c28130f6ce883b175
wjq <1084948949@qq.com>
2024-08-06 09:49:04 +0800

    跨域问题，目前允许所有源

---
+++ src/main/java/com/cyanrocks/wms/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.cyanrocks.wms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Author wjq
+ * @Date 2024/8/6 9:15
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // 允许所有来源
+        config.addAllowedHeader("*"); // 允许所有请求头
+        config.addAllowedMethod("*"); // 允许所有请求方法
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
